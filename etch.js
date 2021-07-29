4186735c3f7c7b1251385f3eac179e68046453e6
xlrn <ncl.yim@gmail.com>
2021-07-29 13:09:28 -0700

    js to change color on hover

--- etch.js
+++ etch.js
@@ -1,10 +1,17 @@
 const container = document.querySelector('#container');
-
-let divs = [];
 const squares = 16;
+
 for (i = 0; i < Math.pow(squares, 2); i++) {
     let tempDiv = document.createElement('div');
     tempDiv.setAttribute('id', `square${i}`);
     tempDiv.setAttribute('class', 'gridsquare');
     container.appendChild(tempDiv);
 }
+
+let divs = document.querySelectorAll('.gridsquare');
+
+divs.forEach(function(e) {
+    e.addEventListener('mouseover', function() {
+        e.style.backgroundColor = 'aqua';
+    });
+});
